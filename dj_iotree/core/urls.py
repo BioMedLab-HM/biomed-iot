6afbb0b0c375d467d1fb2599e82514b192331934
Rene Sesgoer <rene.sesgoer@hm.edu>
2023-09-20 13:50:49 +0200

    core-about url route and basic view created

--- dj_iotree/core/urls.py
+++ dj_iotree/core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='core-home'),
+    path('about/', views.about, name='core-about'),
 ]
--- dj_iotree/core/views.py
+++ dj_iotree/core/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return HttpResponse('<h1>IoTree42 Home</h1>')
+
+def about(request):
+    return HttpResponse('<h1>IoTree42 About</h1>')
