e5d595dbeb0669c2103183caeafc6c03fd3107c7
Rene Sesgoer <rene.sesgoer@hm.edu>
2023-09-21 17:00:50 +0200

    Bare bootstrap theme added project wide; menu bar items added in coreapp base.html

--- dj_iotree/core/views.py
+++ dj_iotree/core/views.py
@@ -11,10 +11,10 @@ def about(request):
     return render(request, 'core/about.html', {'title': 'About'})
 
 def contact_us(request):
-    return render(request, 'core/contact_us.html')
+    return render(request, 'core/contact_us.html', {'title': 'Contact Us'})
 
 def impressum(request):
-    return render(request, 'core/impressum.html')
+    return render(request, 'core/impressum.html', {'title': 'Impressum'})
 
 def datenschutz(request):
-    return render(request, 'core/datenschutz.html')
+    return render(request, 'core/datenschutz.html', {'title': 'Datenschutz'})
