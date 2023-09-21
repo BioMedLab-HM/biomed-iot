025a8cb925a46bfdada081a1f77039ca083bd115
Rene Sesgoer <rene.sesgoer@hm.edu>
2023-09-21 15:32:50 +0200

    reference base template, CDN, no design so far

--- dj_iotree/core/views.py
+++ dj_iotree/core/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Temporärer context
+
+
 def home(request):
     return render(request, 'core/home.html')
 
 def about(request):
-    return render(request, 'core/about.html')
+    return render(request, 'core/about.html', {'title': 'About'})
 
 def contact_us(request):
     return render(request, 'core/contact_us.html')
