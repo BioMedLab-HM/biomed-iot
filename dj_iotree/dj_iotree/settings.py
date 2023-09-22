db86bb7010d5251b1af4753ec2d4e7450f9c8ffb
Rene Sesgoer <rene.sesgoer@hm.edu>
2023-09-22 13:58:23 +0200

    added users app to INSTALLED_APPS

--- dj_iotree/dj_iotree/settings.py
+++ dj_iotree/dj_iotree/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'core.apps.CoreConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
