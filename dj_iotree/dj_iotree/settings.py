7564661ac0bbbc4e19ed5126ca67ccf234282a2b
Rene Sesgoer <rene.sesgoer@hm.edu>
2023-09-27 11:53:03 +0200

    tidied up code

--- dj_iotree/dj_iotree/settings.py
+++ dj_iotree/dj_iotree/settings.py
@@ -135,9 +135,8 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'core-home'
 LOGIN_URL = 'login'
 
-# TODO: setup in gmail etc
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = config["SENDING_PASS"]
