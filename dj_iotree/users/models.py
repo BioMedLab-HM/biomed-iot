6286a297bd24fa2837964cd3bf977f142593a9c1
Rene Sesgoer <rene.sesgoer@hm.edu>
2023-10-20 10:52:23 +0200

    custom User model

--- dj_iotree/users/models.py
+++ dj_iotree/users/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 from PIL import Image
 
+class User(AbstractUser):
+    pass
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
