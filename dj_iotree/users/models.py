0fef1e6a4dbdb110e861e2f04b686e938479103d
Rene Sesgoer <rene.sesgoer@hm.edu>
2023-10-20 12:07:38 +0200

    custom user model deleted

--- dj_iotree/users/models.py
+++ dj_iotree/users/models.py
@@ -1,9 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import User
 from PIL import Image
-
-class User(AbstractUser):
-    pass
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -21,3 +18,4 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+            
