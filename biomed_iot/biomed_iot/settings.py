938b20fedf2d65f8a62e60baf2e96974ab3673bf
Rene Sesgoer <rene.sesgoer@hm.edu>
2024-08-15 01:21:34 +0200

    set Debug = False

--- biomed_iot/biomed_iot/settings.py
+++ biomed_iot/biomed_iot/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config.django.DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [config.host.IP, config.host.HOSTNAME, config.host.DOMAIN, 'localhost', '127.0.0.1']
 
