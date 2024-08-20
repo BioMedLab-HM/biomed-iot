e73b1a5bf4049c3a42a049d1d5f86c4219d32e3f
Rene Sesgoer <rene.sesgoer@hm.edu>
2024-08-20 21:42:11 +0200

    remove comments

--- setup_files/install_02_security_packages.py
+++ setup_files/install_02_security_packages.py
@@ -23,10 +23,6 @@ def install_security_packages(setup_scheme):
 		'sudo ufw allow in from 172.17.0.0/16 to any port 8086',  # InfluxDB access in docker network
 		'sudo ufw allow in from 172.17.0.0/16 to any port 1884',  # Mosquitto access in docker network
 		'sudo ufw allow in from 172.17.0.0/16 to any port 1885',  # Mosquitto access in docker network
-		# Block inter-container communication comes after docker installation 
-		# Allow traffic from containers to host 
-		# Allow traffic from host to containers
-		# TODO: avoid opening the InfluxDB port by implementing docker network to allow influxdb node connect directly to the db
 		# 'ufw allow 3000/tcp',  # For Grafana admin to login to dashboard. If used with TLS, add proxy pass to nginx!
 		'echo',
 		"echo 'To enable ufw firewall, automatic response to confirmation will be provided.'",
