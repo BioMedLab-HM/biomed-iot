2c605641a43d72bbd52f10c8763c4458cd19db6a
Rene Sesgoer <sesgoer.rene@hm.edu>
2025-07-26 08:04:21 +0200

    ssl options inserted

--- setup_files/config/tmp.nginx-biomed-iot-tls-domain.conf.sh
+++ setup_files/config/tmp.nginx-biomed-iot-tls-domain.conf.sh
@@ -17,9 +17,18 @@ server {
     ssl_certificate /etc/ssl/certs/ssl-cert-snakeoil.pem;
     ssl_certificate_key /etc/ssl/private/ssl-cert-snakeoil.key;
 
+    # From https://github.com/certbot/certbot/blob/d8acf7cea0cd3216f38480803a1cc12b6970305b/certbot-nginx/src/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf#L4
+    # Contents are based on https://ssl-config.mozilla.org
+    ssl_session_cache shared:le_nginx_SSL:10m;
+    ssl_session_timeout 1440m;
+    ssl_session_tickets off;
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_prefer_server_ciphers off;
+    ssl_ciphers "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384";
+
     # Custom headers (X‑Frame‑Options, etc.)
     include snippets/nginx_security_options.conf;
-
+    
     location = /favicon.ico { access_log off; log_not_found off; }
 
     # location to Django static files
